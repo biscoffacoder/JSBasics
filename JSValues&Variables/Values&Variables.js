0c7ef03726a09fa351383567157cd5b77f50e4a0
biscoffacoder <biscoffacoder@yahoo.com>
2021-04-26 11:44:53 +0530

    Values&Variables commit

---
+++ JSValues&Variables/Values&Variables.js
@@ -0,0 +1,34 @@
+let numberVal = 90;
+const constNumberVal=99;
+let stringVal= "Allan";
+let anoString = 'Abi';
+let bool = true;
+console.log(stringVal.length);
+console.log(stringVal[0]);
+
+let valo ="Baseball"
+console.log(valo.slice(0,2));
+console.log(valo.slice(2));
+console.log(valo.slice(20));
+
+let fed = valo.replace('ball','bat')
+"123"[1] // 2
+
+let age1 = 90;
+`Hello ${age1}`
+
+let age=null;
+age=9;
+
+let color; 
+console.log(color) // undefined;
+
+let arr = [];
+let val=0;
+for(let r =0; r<10;r++){
+  val=(Math.random()*10);
+  
+ arr.push(Math.floor(val)); 
+}
+let parser = "99cents"
+parseInt(parser) // 99
